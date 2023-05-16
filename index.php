143631b04b244e2b1c6c7ca9f1cf56aca6ea4896
LookasWasTaken~ <piacen.luca@gmail.com>
2023-05-16 16:26:31 +0200

    ADD structure

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ class Movie
     public $vote;
     public $language = "English";
 
-    public function __construct(string $title, string $director, Genre $genre, int $duration, string $vote)
+    public function __construct(string $title, string $director, Genre $genre, int $duration, int $vote)
     {
         $this->title = $title;
         $this->director = $director;
@@ -41,16 +41,21 @@ class Movie
 class Genre
 {
     public $genre;
+
+    public function __construct(string $genre)
+    {
+        $this->genre = $genre;
+    }
 }
 
 $movies = [
-    new Movie("Ready Player One", "Steven Spielberg", new Genre("Fantasy"), 140, "7.4 IMDb"),
-    new Movie("The Lord of The Ring: The Return of the King", "Peter Jackson", new Genre("Fantasy"), 201, "9 IMDb"),
-    new Movie("Avengers: Endgame", "Anthony Russo, Joe Russo", new Genre("Fantasy"), 181, "8.4 IMDb"),
-    new Movie("Zodiac", "David Fincher", new Genre("Thriller"), 157, "7.7 IMDb"),
-    new Movie("The Terminal", "Steven Spielberg", new Genre("Comedy"), 128, "7.4 IMDb"),
-    new Movie("The Imitation Game", "Morten Tyldum", new Genre("Thriller"), 114, "8 IMDb"),
-    new Movie("Fight Club", "David Fincher", new Genre("Thriller"), 139, "8 IMDb")
+    new Movie("Ready Player One", "Steven Spielberg", new Genre("Fantasy"), 140, 7.4),
+    new Movie("The Lord of The Ring: The Return of the King", "Peter Jackson", new Genre("Fantasy"), 201, 9),
+    new Movie("Avengers: Endgame", "Anthony Russo, Joe Russo", new Genre("Fantasy"), 181, 8.4),
+    new Movie("Zodiac", "David Fincher", new Genre("Thriller"), 157, 7.7),
+    new Movie("The Terminal", "Steven Spielberg", new Genre("Comedy"), 128, 7.4),
+    new Movie("The Imitation Game", "Morten Tyldum", new Genre("Thriller"), 114, 8),
+    new Movie("Fight Club", "David Fincher", new Genre("Thriller"), 139, 8)
 ];
 
 ?>
@@ -69,16 +74,33 @@ $movies = [
 
 </head>
 
-<body>
+<body class="bg-dark">
 
-    <div class="container">
-        <h1 class="text-center">MOVIE DATABASE</h1>
-        <div class="row p-4">
+    <div class="container py-5">
+        <h1 class="text-center text-danger">MOVIE DATABASE</h1>
+        <div class="row py-4">
             <?php foreach ($movies as $movie) : ?>
-                <div class="col-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4 class="text-center text-black"><?php echo ($movie->title) ?></h4>
+                <div class="col-8 mx-auto">
+                    <div class="card m-4">
+                        <div class="card-header bg-dark">
+                            <h4 class="text-center text-success"><?php echo ($movie->title) ?></h4>
+                        </div>
+                        <div class="card-body">
+                            <div>
+                                <h6>Director: <?php echo ($movie->director) ?></h6>
+                            </div>
+                            <div>
+                                <h6>Genre: <?php echo ($movie->genre->genre) ?></h6>
+                            </div>
+                            <div>
+                                <h6>Duration: <?php echo ($movie->duration) ?>min</h6>
+                            </div>
+                            <div>
+                                <h6>Average Vote: <?php echo ($movie->vote) ?> from IMDb</h6>
+                            </div>
+                            <div>
+                                <h6>Language: <?php echo ($movie->language) ?></h6>
+                            </div>
                         </div>
                     </div>
                 </div>
