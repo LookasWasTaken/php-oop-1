52527d08cbf287bf0cdfe5dfd6040bd6daa1d0d3
LookasWasTaken~ <piacen.luca@gmail.com>
2023-05-16 16:36:05 +0200

    ADD other movies

--- db.php
+++ db.php
@@ -7,5 +7,12 @@ $movies = [
     new Movie("Zodiac", "David Fincher", new Genre("Thriller"), 157, 7.7),
     new Movie("The Terminal", "Steven Spielberg", new Genre("Comedy"), 128, 7.4),
     new Movie("The Imitation Game", "Morten Tyldum", new Genre("Thriller"), 114, 8),
-    new Movie("Fight Club", "David Fincher", new Genre("Thriller"), 139, 8)
+    new Movie("Fight Club", "David Fincher", new Genre("Thriller"), 139, 8),
+    new Movie("Inception", "Christopher Nolan", new Genre("Science Fiction"), 148, 8.8),
+    new Movie("The Dark Knight", "Christopher Nolan", new Genre("Action"), 152, 9),
+    new Movie("Pulp Fiction", "Quentin Tarantino", new Genre("Crime"), 154, 8.9),
+    new Movie("The Shawshank Redemption", "Frank Darabont", new Genre("Drama"), 142, 9.3),
+    new Movie("Interstellar", "Christopher Nolan", new Genre("Science Fiction"), 169, 8.6),
+    new Movie("The Matrix", "Lana Wachowski, Lilly Wachowski", new Genre("Science Fiction"), 136, 8.7),
+    new Movie("Inglourious Basterds", "Quentin Tarantino", new Genre("War"), 153, 8.3)
 ];
