0c3cde3aa98a835b5040c4f4717ddd97dab1e5a2
LookasWasTaken~ <piacen.luca@gmail.com>
2023-05-16 16:08:44 +0200

    ADD class genre and basic structure html

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class Movie
 {
     public $title;
@@ -8,7 +8,8 @@ class Movie
     public $vote;
     public $language = "English";
 
-    public function __construct(string $title, string $director, string $genre, int $duration, string $vote) {
+    public function __construct(string $title, string $director, Genre $genre, int $duration, string $vote)
+    {
         $this->title = $title;
         $this->director = $director;
         $this->genre = $genre;
@@ -16,7 +17,8 @@ class Movie
         $this->vote = $vote;
     }
 
-    public function getData() {
+    public function getData()
+    {
         return $this->title;
         return $this->director;
         return $this->genre;
@@ -25,7 +27,8 @@ class Movie
         return $this->language;
     }
 
-    public function varDumped(){
+    public function varDumped()
+    {
         var_dump($this->title);
         var_dump($this->director);
         var_dump($this->genre);
@@ -35,13 +38,57 @@ class Movie
     }
 }
 
-$rpo = new Movie ("Ready Player One", "Steven Spielberg", "Fantasy", 140, "7,4 IMDb");
-$lotrtrotk = new Movie ("The Lord of The Ring: The Return of the King", "Peter Jackson", "Fantasy", 201, "9 IMDb");
-$end = new Movie ("Avengers: Endgame", "Anthony Russo, Joe Russo", "Fantasy", 181, "8,4 IMDb");
+class Genre
+{
+    public $genre;
+}
 
-var_dump($rpo);
-var_dump($lotrtrotk);
-var_dump($end);
+$movies = [
+    new Movie("Ready Player One", "Steven Spielberg", new Genre("Fantasy"), 140, "7.4 IMDb"),
+    new Movie("The Lord of The Ring: The Return of the King", "Peter Jackson", new Genre("Fantasy"), 201, "9 IMDb"),
+    new Movie("Avengers: Endgame", "Anthony Russo, Joe Russo", new Genre("Fantasy"), 181, "8.4 IMDb"),
+    new Movie("Zodiac", "David Fincher", new Genre("Thriller"), 157, "7.7 IMDb"),
+    new Movie("The Terminal", "Steven Spielberg", new Genre("Comedy"), 128, "7.4 IMDb"),
+    new Movie("The Imitation Game", "Morten Tyldum", new Genre("Thriller"), 114, "8 IMDb"),
+    new Movie("Fight Club", "David Fincher", new Genre("Thriller"), 139, "8 IMDb")
+];
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie DB</title>
+
+    <!-- EXTERNAL BOOTSTRAP CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <h1 class="text-center">MOVIE DATABASE</h1>
+        <div class="row p-4">
+            <?php foreach ($movies as $movie) : ?>
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="text-center text-black"><?php echo ($movie->title) ?></h4>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+
+    <!-- EXTERNAL BOOTSTRAP JS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
